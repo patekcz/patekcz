0f78deb3804647b84478d2b9f1b2450bef91a63d
patek_cz <52839023+patekcz@users.noreply.github.com>
2024-09-03 16:33:15 +0200

    Update update-readme.js

--- src/update-readme.js
+++ src/update-readme.js
@@ -211,9 +211,10 @@ function formatLanguagesData(languagesData) {
     };
     const icon = iconMapping[lang.name.toLowerCase()] || 'code-blue'; // Výchozí ikona
     const iconUrl = `https://github.com/patekcz/patekcz/raw/main/icon-language/${icon}.png`;
-    return `<span style="display: inline-flex; align-items: center; font-size: 12px; white-space: nowrap; overflow: hidden;"> <img src="${iconUrl}" height="15" alt="${lang.name.toLowerCase()} logo" style="margin-right:4px;" /> **${lang.name.toLowerCase()}**: ${lang.percent.toFixed(2)}%</span>`; // Zajištění jednoho řádku
-  }).join(' | '); // Použití '|' pro oddělení jazyků
-  return `${languages}`;
+    return `<span style="display: inline-flex; align-items: center; font-size: 12px; white-space: nowrap; margin-right: 10px;"> <img src="${iconUrl}" height="15" alt="${lang.name.toLowerCase()} logo" style="margin-right: 4px;" /> **${lang.name.toLowerCase()}**: ${lang.percent.toFixed(2)}%</span>`; // Zajištění jednoho řádku
+  }).join(''); // Bez oddělení
+
+  return `<div style="display: flex; flex-wrap: wrap; max-width: 100%;">${languages}</div>`; // Flexbox pro zalamování
 }
 
 function generateWakaTimeContent(allTimeData, timeData) {
